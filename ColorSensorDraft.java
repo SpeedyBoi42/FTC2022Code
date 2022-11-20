0dcd0714cf8f94eadb5125f7dcc7b4f41cee7193
SpeedyBoi42 <118115120+SpeedyBoi42@users.noreply.github.com>
2022-11-20 16:28:42 -0500

    Update ColorSensorDraft.java

--- ColorSensorDraft.java
+++ ColorSensorDraft.java
@@ -23,11 +23,10 @@ public class ColorSensorDraft extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
 
     //constants for driving motors
-    //some of these numbers def need changing, like diameter
     //for the counts per motor rev, i found 560 in the documentation for our specific motors we use
-    static final double     COUNTS_PER_MOTOR_REV    = 560 ;    // eg: TETRIX Motor Encoder
-    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // No External Gearing.
-    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_MOTOR_REV    = 560 ; 
+    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;  
+    static final double     WHEEL_DIAMETER_INCHES   = 2.95276 ; 
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
             (WHEEL_DIAMETER_INCHES * Math.PI);
     static final double     DRIVE_SPEED             = 0.6;
@@ -76,45 +75,17 @@ public class ColorSensorDraft extends LinearOpMode {
         runtime.reset();
 
         //trying to move with encoders :/
-        
+
         //the exact inches are off but the overall idea here is to move forward to the cone
         //   and then strafe to the right because i was planning on putting the color sensor in the
         //   front left corner so it doesn't come close to causing any issues with the picker upper
-        
-        //       (also, there is a calculation to figure out how many inches to move based on the angle you want to go
-        //           you take the sin of the angle, so sin(90) = 1. and then you multiply by i think the width of the robot
-        //           so for this, if you wanted to turn 90 degrees, you would be moving roughly 17 inches 
-        //           but i'm bypassing the annoying process of turning and then turning back by just strafing into position)
-        
+
         encoderDrive(DRIVE_SPEED, 24, 24, 24, 24, 5.0);
         encoderDrive(TURN_SPEED, 8.5, -8.5, -8.5, 8.5, 4.0);
 
-        /* NOT USING THIS (just a hardcoded part in case we cant get the encoders to work,
-               but this still doesn't have the right time values)
-        //move forward (w/o encoder)
-        while (runtime.seconds() < 1.5){
-            frontRight.setPower(0.7);
-            frontLeft.setPower(0.7);
-            backRight.setPower(0.7);
-            backLeft.setPower(0.7);
-        }
-
-        //reset runtime again
-        runtime.reset();
-        //move right
-        while (runtime.seconds() < 0.4){
-            frontRight.setPower(-0.6);
-            frontLeft.setPower(0.6);
-            backRight.setPower(0.6);
-            backLeft.setPower(-0.6);
-        }
-
-         */
-
-
         //shows the color values that color sensor can see
         //so in theory, this would show the color values that the sensor sees at the time it was read
-        //   this can be used to see what should be in those if statments below cuz idk where 20 came from
+        //   this can be used to see what should be in those if statements below cuz idk where 20 came from
         telemetry.addData("red", color.red());
         telemetry.addData("green", color.blue());
         telemetry.addData("blue", color.green());
@@ -160,6 +131,67 @@ public class ColorSensorDraft extends LinearOpMode {
         //making sure this should actually be running
         if (opModeIsActive()) {
             //setting the target position
+            newFRTarget = frontRight.getCurrentPosition() + (int)(frightInches * COUNTS_PER_INCH);
+            newFLTarget = frontLeft.getCurrentPosition() + (int)(fleftInches * COUNTS_PER_INCH);
+            newBRTarget = backRight.getCurrentPosition() + (int)(brightInches * COUNTS_PER_INCH);
+            newBLTarget = backLeft.getCurrentPosition() + (int)(bleftInches * COUNTS_PER_INCH);
+
+            frontRight.setTargetPosition(newFRTarget);
+            frontLeft.setTargetPosition(newFLTarget);
+            backRight.setTargetPosition(newBRTarget);
+            backLeft.setTargetPosition(newBLTarget);
+
+            //making the motor mode run to position
+            frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            //resetting runtime
+            runtime.reset();
+            //setting the power of the motors to get it to move
+            frontRight.setPower(Math.abs(speed));
+            frontLeft.setPower(Math.abs(speed));
+            backRight.setPower(Math.abs(speed));
+            backLeft.setPower(Math.abs(speed));
+
+            //making sure it does stop until it reaches
+            while (opModeIsActive() && (runtime.seconds() < timeoutS) &&
+                    (frontRight.isBusy() || frontLeft.isBusy() || backRight.isBusy() || backLeft.isBusy())){
+                telemetry.addLine("We should be moving, the motors should be turning if you see this.");
+                telemetry.update();
+            }
+
+            //stopping all movement
+            frontRight.setPower(0);
+            frontLeft.setPower(0);
+            backRight.setPower(0);
+            backLeft.setPower(0);
+
+            //changing the motor mode *again*
+            frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+        }
+
+    }
+    public void encoderDrive(double speed, double angle, double timeoutS) {
+        //intializing variables
+        int newFRTarget;
+        int newFLTarget;
+        int newBRTarget;
+        int newBLTarget;
+
+        //making sure this should actually be running
+        if (opModeIsActive()) {
+            //setting the target position
+            double frightInches = (Math.sin(angle)*17);
+            double fleftInches = -(Math.sin(angle)*17);
+            double brightInches = (Math.sin(angle)*17);
+            double bleftInches = -(Math.sin(angle)*17);
             newFRTarget = frontRight.getCurrentPosition() + (int)(frightInches * COUNTS_PER_INCH);
             newFLTarget = frontLeft.getCurrentPosition() + (int)(fleftInches * COUNTS_PER_INCH);
             newBRTarget = backRight.getCurrentPosition() + (int)(brightInches * COUNTS_PER_INCH);
